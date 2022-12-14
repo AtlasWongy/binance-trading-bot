ef38bd300d04f8eb511c8353706685284fcbbc8d
AtlasWongy <atlasyjyj@gmail.com>
2022-12-14 19:58:10 +0800

    update_main_branch
    Attempt Visual display of Bollinger Bands on ETHBUSD

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from binance.client import Client
 import json
+import requests
+import pandas as pd
 
 f = open('config.json')
-api_data = json.load(f)
+config = json.load(f)
 
-client = Client(api_data["api_key"], api_data["secure_key"])
+client = Client(config["api_key"], config["secure_key"])
 print("Connected to Binance successfully....")
 
-info = client.get_symbol_info('BNBBTC')
+klines = client.get_historical_klines(config["currency"], Client.KLINE_INTERVAL_5MINUTE, "1 day ago UTC")
 
-for i in info:
-    print(i)
+klines_dataframe = pd.DataFrame(
+    klines, 
+    columns=[
+        'Open Time', 
+        'Open', 
+        'High', 
+        'Low', 
+        'Close', 
+        'Volume',
+        'Close Time',
+        'Quote Asset',
+        'Volume',
+        'Number of Trades',
+        'Taker buy base asset volume',
+        'Taker buy quote asset volume'
+    ])
+
+print(klines_dataframe)
+
+
+
+
