5495156afe82b520f2506d442dd57a522e59c713
AtlasWongy <atlasyjyj@gmail.com>
2022-12-20 10:56:04 +0800

    update
    Add in interval ping and re-establish conn

--- real_time_data_stream.py
+++ real_time_data_stream.py
@@ -1,23 +1,63 @@
 import asyncio
 import json
+import time
+import websocket
 from binance import AsyncClient, BinanceSocketManager
-    
+import datetime
+import timeit
+import ast
+import os
+import sys
 
-async def main():
-    client = await AsyncClient.create()
-    bm = BinanceSocketManager(client, user_timeout = 60)
-    f = open('config.json')
-    config = json.load(f)
+def on_message(ws, message):
+    response = json.loads(message)
+    print(response)
+    print("-------------------------------------")
 
-    ts = bm.kline_socket(config['currency'], AsyncClient.KLINE_INTERVAL_5MINUTE)
-    async with ts as tscm:
-        while True:
-            res = await tscm.recv()
-            print("---------------------------------------------")
-            print(res)
+def on_error(ws, error):
+    print(error)
 
-    await client.close_connection()
+def on_close(ws, status_code, msg):
+    print("WebSocket connection closed")
+    # Un-comment line below to restart python script
+    # os.execv(sys.executable, [sys.executable] + sys.argv)
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    f = open('config.json')
+    config = json.load(f)
+    ws = websocket.WebSocketApp(f"wss://stream.binance.com/ws/{config['currency']}@kline_5m",
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close,
+                            )
+    ws.run_forever(ping_interval=300)
+
+
+# f = open('config.json')
+# config = json.load(f)
+# stream_k_lines(config['currency'], '5m')
+
+# async def main():
+#     client = await AsyncClient.create()
+#     bm = BinanceSocketManager(client, user_timeout = 60)
+#     f = open('config.json')
+#     config = json.load(f)
+#     ts = bm.kline_socket(config['currency'], AsyncClient.KLINE_INTERVAL_5MINUTE) #Change here
+#     async with ts as tscm:
+#         while True:
+#             start = time.perf_counter()
+#             res = await tscm.recv()
+#             end = time.perf_counter()
+            
+#             print("---------------------------------------------")
+            
+#             print(res)
+#             print(" ")
+#             print(f"API request took {end - start:.2f} seconds")
+            
+
+#     await client.close_connection()
+
+# if __name__ == "__main__":
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(main())
